6c5f338b1da695fa8274c71d2ec5eecd45c6b993
Isabella <wileyisabella@gmail.com>
2021-03-23 20:46:24 -0400

    commit tree 665d1aaad91e2c6f92ce3d7fad9139aec14bc7e8

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    return Math.abs(42 - street)
+}
+
+function distanceFromHqInFeet(street){
+    return(distanceFromHqInBlocks(street)*264)
+}
+
+function distanceTravelledInFeet(street1, street2){
+    return((Math.abs(street1 - street2))*264)
+}
+
+function calculatesFarePrice(street1, street2){
+    let distance = distanceTravelledInFeet(street1, street2)
+    if (distance < 400){
+        return 0
+    }
+    else if(distance <= 2000){
+        return ((distance - 400)/50)
+    }
+    else if(distance < 2500){
+        return 25
+    }
+    else{
+        return(`cannot travel that far`)
+    }
+}
+
